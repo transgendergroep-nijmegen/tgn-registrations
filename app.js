783b9507f59079f6982192a5494a101b4a9303cf
Marein <mail@marein.net>
2020-09-24 21:03:12 +0200

    client practically done

--- app.js
+++ app.js
@@ -6,160 +6,284 @@ firebase.initializeApp({
 
 angular
   .module("tgn-registrations", ["firebase"])
-  .controller("RegistrationsCtrl", ($scope, $firebaseAuth, $firebaseArray) => {
-    $scope.authObj = $firebaseAuth();
-    $scope.events = $firebaseArray(firebase.database().ref("events"));
-    $scope.users = $firebaseArray(firebase.database().ref("users"));
+  .controller(
+    "RegistrationsCtrl",
+    (
+      $scope,
+      $interval,
+      $filter,
+      $firebaseAuth,
+      $firebaseObject,
+      $firebaseArray
+    ) => {
+      $scope.authObj = $firebaseAuth();
+      $scope.events = $firebaseArray(firebase.database().ref("events"));
+      $scope.users = $firebaseObject(firebase.database().ref("users"));
+      $scope.usersArray = $firebaseArray(firebase.database().ref("users"));
 
-    $scope.authObj.$onAuthStateChanged((user) => {
-      $scope.user = user;
-      if (user)
-        $firebaseArray(
-          firebase.database().ref(`users/${user.uid}/info_history`)
-        )
-          .$loaded()
-          .then((userInfoHistory) => {
-            $scope.userInfo = userInfoHistory[userInfoHistory.length - 1];
-          });
-    });
+      $interval(() => {
+        $scope.now = Date.now();
+      }, 1000);
 
-    $scope.register = (form) => {
-      $scope.authObj
-        .$createUserWithEmailAndPassword(form.email, form.password)
-        .then((user) => {
-          user.sendEmailVerification();
-          $scope.setUserInfo(user, {
-            email: form.email,
-            name: form.name,
-            phone: form.phone,
-          });
-          $scope.showTab("events");
-        })
-        .catch($scope.show_toast);
-    };
+      $scope.authObj.$onAuthStateChanged((user) => {
+        $scope.user = user;
+        if (user) {
+          // Set $scope.userInfo.
+          $firebaseObject(firebase.database().ref(`users/${user.uid}`))
+            .$loaded()
+            .then((data) => {
+              $scope.userInfo = data;
+            });
 
-    $scope.forgotpassword = (form) => {
-      if (!form.email) {
-        $scope.show_toast(
-          "Vul een e-mailadres in om je wachtwoord te resetten."
-        );
-      } else {
+          // Start email verification check routine.
+          let emailVerificationInterval = $interval(() => {
+            $scope.user?.reload().then(() => {
+              if ($scope.user?.emailVerified) {
+                $scope.$apply();
+                $interval.cancel(emailVerificationInterval);
+              }
+            });
+          }, 1000);
+        }
+      });
+
+      $scope.register = (form) => {
+        $scope.working = true;
         $scope.authObj
-          .$sendPasswordResetEmail(form.email)
+          .$createUserWithEmailAndPassword(form.email, form.password)
           .then((user) => {
+            user.sendEmailVerification();
+            $scope.setUserInfo(user, {
+              email: form.email,
+              name: form.name,
+              phone: form.phone,
+            });
+            $scope.showTab("events");
             $scope.show_toast(
-              "Er is naar het ingevulde adres een e-mail verstuurd met instructies om je wachtwoord te resetten."
+              "Account aangemaakt. Er is naar het ingevulde adres een e-mail verstuurd met instructies om de registratie te voltooien."
             );
+            $scope.working = false;
           })
           .catch($scope.show_toast);
-      }
-    };
+      };
 
-    $scope.show_toast = (text) => {
-      $scope.toast_text = text;
-      $(".toast").toast("show");
-    };
-
-    $scope.signin = (form) => {
-      $scope.authObj
-        .$signInWithEmailAndPassword(form.email, form.password)
-        .then((user) => {
-          $scope.showTab("events");
-        })
-        .catch($scope.show_toast);
-    };
-
-    $scope.signout = () => {
-      $scope.authObj.$signOut();
-      $scope.showTab("events");
-    };
-
-    $scope.showTab = (tab) => {
-      $('.nav-tabs a[href="#' + tab + '"]').tab("show");
-    };
-
-    $scope.assign = (varName, value) => {
-      $scope[varName] = value;
-    };
-
-    $scope.setinfo = (form) => {
-      $scope.authObj
-        .$signInWithEmailAndPassword($scope.user.email, form.password)
-        .then((user) => {
+      $scope.forgotpassword = (form) => {
+        if (!form.email) {
+          $scope.show_toast(
+            "Vul een e-mailadres in om je wachtwoord te resetten."
+          );
+        } else {
+          $scope.working = true;
           $scope.authObj
-            .$updateEmail(form.email)
-            .then(() => {
-              $scope.setUserInfo($scope.user, {
-                email: form.email,
-                name: form.name,
-                phone: form.phone,
-              });
-              $scope.setView("main");
+            .$sendPasswordResetEmail(form.email)
+            .then((user) => {
+              $scope.show_toast(
+                "Er is naar het ingevulde adres een e-mail verstuurd met instructies om je wachtwoord te resetten."
+              );
+              $scope.working = false;
             })
             .catch($scope.show_toast);
-        })
-        .catch($scope.show_toast);
-    };
+        }
+      };
 
-    $scope.setUserInfo = (user, userInfo) => {
-      let userInfoHistory = $firebaseArray(
-        firebase.database().ref(`users/${user.uid}/info_history`)
-      );
-      userInfoHistory.$add(userInfo);
-      $scope.userInfo = userInfo;
-    };
+      $scope.show_toast = (text) => {
+        $scope.toast_text = text;
+        $(".toast").toast("show");
+      };
 
-    $scope.setpassword = (form) => {
-      $scope.authObj
-        .$signInWithEmailAndPassword($scope.user.email, form.password.old)
-        .then((user) => {
-          $scope.authObj
-            .$updatePassword(form.password.new)
-            .then(() => {
-              $scope.setView("main");
-            })
-            .catch($scope.show_toast);
-        })
-        .catch($scope.show_toast);
-    };
+      $scope.signin = (form) => {
+        $scope.working = true;
+        $scope.authObj
+          .$signInWithEmailAndPassword(form.email, form.password)
+          .then((user) => {
+            $scope.showTab("events");
+            $scope.working = false;
+          })
+          .catch($scope.show_toast);
+      };
 
-    $scope.deleteaccount = (form) => {
-      $scope.authObj
-        .$signInWithEmailAndPassword($scope.user.email, form.password)
-        .then((user) => {
-          $scope.authObj
-            .$deleteUser()
-            .then(() => {
-              $scope.setView("main");
-            })
-            .catch($scope.show_toast);
-        })
-        .catch($scope.show_toast);
-    };
+      $scope.signout = () => {
+        $scope.authObj.$signOut();
+        $scope.showTab("events");
+      };
 
-    $scope.set_user_event_status = (user, event, status) => {
-      firebase
-        .database()
-        .ref(`events/${event.$id}/signups/${user.uid}`)
-        .set(status);
-    };
+      $scope.setinfo = (form) => {
+        $scope.working = true;
+        $scope.authObj
+          .$signInWithEmailAndPassword($scope.user.email, form.password)
+          .then((user) => {
+            $scope.authObj
+              .$updateEmail(form.email)
+              .then(() => {
+                user.sendEmailVerification();
+                $scope.setUserInfo($scope.user, {
+                  email: form.email,
+                  name: form.name,
+                  phone: form.phone,
+                });
+                $scope.showTab("events");
+                $scope.working = false;
+              })
+              .catch($scope.show_toast);
+          })
+          .catch($scope.show_toast);
+      };
 
-    $scope.count_event_signups = (event) => {
-      return event.fake_signups + Object.keys(event.signups ?? []).length;
-    };
+      $scope.setUserInfo = (user, userInfo) => {
+        $scope.working = true;
+        firebase
+          .database()
+          .ref(`users/${user.uid}`)
+          .set(userInfo)
+          .then(() => {
+            $scope.userInfo = userInfo;
+            $scope.working = false;
+          })
+          .catch($scope.show_toast);
+      };
 
-    $scope.setForm = (value) => {
-      $scope.form = value;
+      $scope.setpassword = (form) => {
+        $scope.working = true;
+        $scope.authObj
+          .$signInWithEmailAndPassword($scope.user.email, form.password.old)
+          .then((user) => {
+            $scope.authObj
+              .$updatePassword(form.password.new)
+              .then(() => {
+                $scope.showTab("events");
+                $scope.working = false;
+              })
+              .catch($scope.show_toast);
+          })
+          .catch($scope.show_toast);
+      };
+
+      $scope.set_user_event_status = (uid, event, status) => {
+        $scope.working = true;
+        let date_text = $filter("date")(event.date * 1000, "dd/MM/yyyy");
+        let you_are = uid === $scope.user.uid ? "Je bent" : "De account is";
+        if (status)
+          $scope.show_toast(
+            `${you_are} nu ingeschreven voor '${event.name}' op ${date_text}.`
+          );
+        else
+          $scope.show_toast(
+            `${you_are} nu uitgeschreven voor '${event.name}' op ${date_text}.`
+          );
+        firebase
+          .database()
+          .ref(`events/${event.$id}/signups/${uid}`)
+          .set(status)
+          .then(() => {
+            $scope.working = false;
+            $scope.$apply();
+          })
+          .catch($scope.show_toast);
+      };
+
+      $scope.newevent = (event) => {
+        $scope.working = true;
+        $scope.events
+          .$add(event)
+          .then(() => {
+            let date_text = $filter("date")(event.date * 1000, "dd/MM/yyyy");
+            $scope.show_toast(
+              `Activiteit '${event.name}' op ${date_text} is aangemaakt.`
+            );
+            $scope.form = null;
+            $scope.working = false;
+          })
+          .catch($scope.show_toast);
+      };
+
+      $scope.editevent = (event) => {
+        $scope.working = true;
+        let index = $scope.events.$indexFor(event.$id);
+        $scope.events[index] = event;
+        $scope.events
+          .$save(index)
+          .then(() => {
+            let date_text = $filter("date")(event.date * 1000, "dd/MM/yyyy");
+            $scope.show_toast(
+              `Activiteit '${event.name}' op ${date_text} is gewijzigd.`
+            );
+            $scope.working = false;
+          })
+          .catch($scope.show_toast);
+      };
+
+      $scope.deleteevent = (event) => {
+        $scope.working = true;
+        let index = $scope.events.$indexFor(event.$id);
+        $scope.events
+          .$remove(index)
+          .then(() => {
+            let date_text = $filter("date")(event.date * 1000, "dd/MM/yyyy");
+            $scope.show_toast(
+              `Activiteit '${event.name}' op ${date_text} is verwijderd.`
+            );
+            $scope.edit_event = null;
+            $scope.working = false;
+          })
+          .catch($scope.show_toast);
+      };
+
+      $scope.edituser = (user) => {
+        let index = $scope.usersArray.$indexFor(user.$id);
+        $scope.usersArray[index] = user;
+        $scope.usersArray
+          .$save(index)
+          .then(() => {
+            $scope.show_toast(`Account '${user.email}' is gewijzigd.`);
+          })
+          .catch($scope.show_toast);
+      };
+
+      $scope.deleteaccount = (user) => {
+        let index = $scope.users.$indexFor(user.$id);
+        $scope.users
+          .$remove(index)
+          .then(() => {
+            let date_text = $filter("date")(event.date * 1000, "dd/MM/yyyy");
+            $scope.show_toast(
+              `Activiteit '${event.name}' op ${date_text} is verwijderd.`
+            );
+            $scope.edit_event = null;
+          })
+          .catch($scope.show_toast);
+      };
+
+      $scope.showTab = (tab) => {
+        $('.nav-tabs a[href="#' + tab + '"]').tab("show");
+      };
+
+      $scope.assign = (varName, value) => {
+        $scope[varName] = value;
+      };
+
+      $scope.count_event_signups = (event) =>
+        (event.fake_signups ?? 0) + Object.keys(event.signups ?? []).length;
+
+      $scope.eventListName = (event) =>
+        `${$filter("date")(event.date * 1000, "dd/MM/yyyy")} - ${event.name}`;
+
+      $scope.copy = (obj) => Object.assign({}, obj);
+    }
+  )
+  .directive("bindTimestamp", function () {
+    return {
+      restrict: "A",
+      require: "ngModel",
+      link: function (scope, element, attrs, ngModel) {
+        if (ngModel) {
+          ngModel.$parsers.push(function (value) {
+            return Math.floor(new Date(value).getTime() / 1000);
+          });
+
+          ngModel.$formatters.push(function (value) {
+            return new Date(value * 1000);
+          });
+        }
+      },
     };
   });
-
-// jQuery(document).ready(($) => {
-//   // Add functionality to de-active navbar links after clicking.
-//   $("[data-toggle=tab]").click(function () {
-//     if ($(this).hasClass("active")) {
-//       $(this).removeClass("active"); // disable navbar link
-//       $($(this).attr("href")).removeClass("active"); // disable tab content
-//       return false; // keep link from being activated by the same click
-//     }
-//   });
-// });
